9495767e598e6cd118def4340cfec07d9627a5b6
ViniciusEmmanuel <vinicius.teixeira.m@gmail.com>
2019-04-02 10:18:32 -0300

    Calc finzalada

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculadora msg="Calculadora do Vinicius"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculadora from './components/Calculadora'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Calculadora
   }
 }
 </script>
